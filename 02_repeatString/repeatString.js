59c55622abde2ec2f7d41baaeaf39f4f872251c7
Patrick Snijder <76705330+psnijder@users.noreply.github.com>
2024-09-23 14:21:15 +0200

    Pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        let result = '';
+        for (let i = 0; i < num; i++) {
+            result += string
+            console.log(result);
+        };
+        return result;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
